c8b1ee87a54b10ba0eaf95074fdb35a470c4fa5a
havin fahru <havindi.fahru@tonjoo.com>
2026-02-24 14:46:09 +0700

    add login auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // dashboard pages
 Route::get('/', function () {
